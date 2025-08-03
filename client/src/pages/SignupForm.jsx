5a552bd1ef23b5fcb43e415deef6a5e2dd1aaea3
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-08-03 11:28:31 +0530

    ts pmo2

--- client/src/pages/SignupForm.jsx
+++ client/src/pages/SignupForm.jsx
@@ -17,7 +17,7 @@ function SignupForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register"||`${import.meta.env.VITE_API_BASE_URL}/api/auth/register`, {
+      const res = await fetch("http://localhost:5000/api/auth/register"||`${import.meta.env.VITE_API_BASE_URL}/api/auth/register`,   {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
